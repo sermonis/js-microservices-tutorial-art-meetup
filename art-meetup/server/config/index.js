d55e356d42bb38d3dfe8475e927808565f022d83
woodell <dev@sermonis.ru>
2024-03-04 20:06:52 +0300

    main app started

---
+++ art-meetup/server/config/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+	development: {
+
+		sitename: 'Art Meetups [Development]',
+		serviceRegistryUrl: "http://localhost:3000",
+		serviceVersion: "1.x.x",
+
+	},
+
+	production: {
+
+		sitename: 'Art Meetups',
+		serviceRegistryUrl: "http://localhost:3000",
+		serviceVersion: "1.x.x",
+
+	},
+
+};
